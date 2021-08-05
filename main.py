47943e544247972c495a019790bb83997e40c5bc
VigneshM20 <88494825+loki8454@users.noreply.github.com>
2021-08-05 13:22:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+print("welcome to Vignesh pizza sport!")
+size = input("which kind of size do u want? S, M, or L")
+add_pepperoni = input("you want pepperoni? Y or N")
+extra_cheese = input("add extra cheese ? Y or N")
+
+bill = 0
+if size == "S":
+    bill += 15
+elif size == "M":
+    bill += 20
+elif size == "L":
+    bill += 25
+
+if add_pepperoni == "Y":
+    if size == "S":
+        bill += 2
+    else:
+        bill += 3
+
+if extra_cheese == "Y":
+    bill += 1
+
+print(f"the final bill is{bill}")
